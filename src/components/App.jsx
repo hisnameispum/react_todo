fe9dd376b509805f2abc297210a106a2cb783c2c
sirasit <sirasit@siruxsolutions.com>
2022-01-23 12:03:20 -0800

    Finished

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,41 +1,44 @@
-import React, {useState} from "react";
-import TodoItem from "./TodoItem";
+import React, { useState } from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+import CreateArea from "./CreateArea";
 
 function App() {
-  const [inputText, setInputText] = useState("");
-  const [todoList, addTodo] = useState([]);
+    const [notes, setNotes] = useState([]);
 
-  function handleChange(event){
-      const newValue = event.target.value;
-      setInputText(newValue);
-  }
+    function addNote(newNote) {
+        setNotes(prevNotes => {
+            return [...prevNotes, newNote];
+        });
+    }
 
-  function addItem(){
-      addTodo(prevItems => {
-          return [...prevItems, inputText];
-      });
-      setInputText("");
-  }
+    function deleteNote(id) {
+        setNotes(prevNotes => {
+            return prevNotes.filter((noteItem, index) => {
+                return index !== id;
+            });
+        });
+    }
 
-
-  return (
-    <div className="container">
-      <div className="heading">
-        <h1>To-Do List</h1>
-      </div>
-      <div className="form">
-        <input type="text" onChange={handleChange} value={inputText}/>
-        <button onClick={addItem}>
-          <span>Add</span>
-        </button>
-      </div>
-      <div>
-        <ul>
-            {todoList.map(todoItem => <TodoItem todoItem={todoItem} />)}
-        </ul>
-      </div>
-    </div>
-  );
+    return (
+        <div>
+            <Header />
+            <CreateArea onAdd={addNote} />
+            {notes.map((noteItem, index) => {
+                return (
+                    <Note
+                        key={index}
+                        id={index}
+                        title={noteItem.title}
+                        content={noteItem.content}
+                        onDelete={deleteNote}
+                    />
+                );
+            })}
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
